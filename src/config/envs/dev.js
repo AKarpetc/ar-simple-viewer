22837f1de5b0f60bc414d9892aa6959ed729d8f1
Любителев Максим <Lyubitelev@hotmail.com>
2024-09-29 16:39:02 +0700

    updated cloud params

--- src/config/envs/dev.js
+++ src/config/envs/dev.js
@@ -4,9 +4,9 @@ const  conf =
 {
     idsFolder: "dev",
     awsRegion: "ru-central1",
-    awsEndPoint: "https://storage.yandexcloud.kz",
-    awsAccessKeyId: "YCB2BzjHd8epgNyLs6eDfFnW4",
-    awsSecretAccessKey: "YCMVWV1P3UxcIG-YJEcCVyq74jw49_-23UP6UQdT",
+    awsEndPoint: "https://storage.yandexcloud.net",
+    awsAccessKeyId: "",
+    awsSecretAccessKey: "",
     smtpApiUrl: "https://titan-auto-barnaul.ru:5000"
 }
 export default conf;
